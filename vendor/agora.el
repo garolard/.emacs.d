47284ad41b140931555956ba9bf97175e19b39d6
garolard <gbril9119@gmail.com>
2018-04-09 10:15:28 +0200

    Cambiada ruta de trabajo de agora

--- vendor/agora.el
+++ vendor/agora.el
@@ -2,7 +2,7 @@
 
 
 ;; Base folders for restaurant and retail development
-(setq ag:rest-base-dir "d:/desarrollo/igt.pos-devel/")
+(setq ag:rest-base-dir "d:/desarrollo/igt.pos-4.5.x/")
 (setq ag:retail-base-dir "d:/desarrollo/igt.pos.retail-devel/")
 
 
