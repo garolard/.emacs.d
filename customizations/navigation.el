7150381d52299248d39053ae270a0c3f15023047
garolard <gbril9119@gmail.com>
2018-04-09 10:35:05 +0200

    Agregados todos los paquetes de elpa y creados keybindings para neotree

--- customizations/navigation.el
+++ customizations/navigation.el
@@ -65,4 +65,22 @@
 
 ;; neotree - menu lateral para ver archivos
 (require 'neotree)
-(global-set-key [f8] 'neotree-toggle)
+
+;; show current file when opened
+(setq neo-smart-open t) 
+;; set neotree root to projectile root
+(defun neotree-project-dir ()
+  "Open NeoTree using the git root."
+  (interactive)
+  (let ((project-dir (projectile-project-root))
+		(file-name (buffer-file-name)))
+	(neotree-toggle)
+	(if project-dir
+		(if (neo-global--window-exists-p)
+			(progn
+			  (neotree-dir project-dir)
+			  (neotree-find file-name)))
+	  (message "Could not find git project root."))))
+
+(global-set-key [f8] 'neotree-project-dir)
+(global-set-key (kbd "M-l") (lambda () (interactive) (neotree-find (buffer-file-name))))
