a98678a81cffbb7296adc722226c6b9ac19f7a67
Gabriel Ferreiro <gbril9119@gmail.com>
2018-04-10 09:58:02 +0200

    Downgraded Emacs
    Instalada versión 25.3.1 (tenía instalada una beta que hacía que los paquetes no fueran compatibles con la otra versión instalada).
    
    Reinstalados los paquetes que faltaban, ahora deberían funcionar en casa.

--- init.el
+++ init.el
@@ -4,16 +4,16 @@
 
 ;; Define package repositories
 (require 'package)
-;;(add-to-list 'package-archives
-;;             '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(add-to-list 'package-archives
+             '("marmalade" . "https://marmalade-repo.org/packages/") t)
 (add-to-list 'package-archives
              '("tromey" . "http://tromey.com/elpa/") t)
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.milkbox.net/packages/") t)
 
-;; (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-;;                          ("marmalade" . "http://marmalade-repo.org/packages/")
-;;                          ("melpa" . "http://melpa-stable.milkbox.net/packages/")))
+(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+                         ("marmalade" . "https://marmalade-repo.org/packages/")
+                         ("melpa" . "http://melpa-stable.milkbox.net/packages/")))
 
 
 ;; Load and activate emacs packages. Do this first so that the
