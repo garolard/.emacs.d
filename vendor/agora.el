3f1622398a617d24770d899d032ed927a1df3e88
Gabriel Ferreiro <gabriel.ferreiro@igt.es>
2018-10-26 10:41:25 +0200

    Modificado .gitignore con la esperanza de que no trague tanta mierda

--- vendor/agora.el
+++ vendor/agora.el
@@ -2,7 +2,7 @@
 
 
 ;; Base folders for restaurant and retail development
-(setq ag:rest-base-dir "d:/desarrollo/igt.pos.migrate-2.6.x/")
+(setq ag:rest-base-dir "d:/desarrollo/igt.pos-devel/")
 (setq ag:retail-base-dir "d:/desarrollo/igt.pos.retail-devel/")
 
 
