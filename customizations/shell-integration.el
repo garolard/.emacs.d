7e13cf8c0e0c1ca15babaee3a805dbb3fbfc28bc
Gabriel Ferreiro <gabriel.ferreiro@igt.es>
2018-11-06 10:41:20 +0100

    Integración de Git Agregada función para integrar 'de aquella manera' Git en un shell de emacs

--- customizations/shell-integration.el
+++ customizations/shell-integration.el
@@ -4,3 +4,13 @@
   (exec-path-from-shell-initialize)
   (exec-path-from-shell-copy-envs
    '("PATH")))
+
+;; git-bash pseudo-integration
+(defun git-bash () 
+  (interactive)
+  (let ((explicit-shell-file-name "C:/Program Files/Git/bin/bash.exe"))
+    (call-interactively 'shell)
+    (rename-buffer "*git*")
+    (toggle-truncate-lines)
+    (insert "export PS1='\\[\\033[32m\\]\\u@\\h \\[\\033[33m\\]\\w\\[\\033[36m\\]`__git_ps1`\\[\\033[0m\\]\\n$ '")
+    (comint-send-input nil t)))
