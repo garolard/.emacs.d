25c979d6fa2af5775d764e726afebbdaf1d69a57
Gabriel Ferreiro <gabriel.ferreiro@igt.es>
2018-11-07 13:19:43 +0100

    Tocada config de python Se elimina configuracion para C#, total no lo usaba nada.... Se retoca la configuracion de python siguiendo los consejos de esta web: https://realpython.com/emacs-the-best-python-editor/ Tengo que acordarme de modificar el archivo python.el para que enganche el flycheck-mode y modifique el elpy-modules. Tambien he quitado codigo que realmente no sabía que narices estaba haciendo y se mete un paquete (autopep8) para corregir temas de formato. Hay que instalarlo tambien con pip.

--- customizations/lang/python.el
+++ customizations/lang/python.el
@@ -1,9 +1,7 @@
-;; Supuestamente con esto basta...
-(when (require 'elpy nil t)
-  (elpy-enable))
-(setq elpy-rpc-backend "jedi")
-(when (load "flycheck" t )
-  (setq elpy-modules (delq 'elpy-module-flycheck elpy-modules))
+(elpy-enable)
+(when (require 'flycheck nil t)
+  (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
   (add-hook 'elpy-mode-hook 'flycheck-mode))
-(setq python-shell-interpreter "python"
-      python-shell-interpreter-args "-i")
+
+(require 'py-autopep8)
+(add-hook 'elpy-mode-hook 'py-autopep8-enable-on-save)
