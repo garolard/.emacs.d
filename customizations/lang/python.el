62f5a97444ac57e2357475c9fd5e454602b1d4d2
Gabriel Ferreiro <gabriel.ferreiro@igt.es>
2018-11-07 15:23:09 +0100

    Agregados comentarios en python.el

--- customizations/lang/python.el
+++ customizations/lang/python.el
@@ -1,7 +1,9 @@
 (elpy-enable)
 (when (require 'flycheck nil t)
+  (print "Cargando flycheck para elpy")
   (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
   (add-hook 'elpy-mode-hook 'flycheck-mode))
 
 (require 'py-autopep8)
 (add-hook 'elpy-mode-hook 'py-autopep8-enable-on-save)
+(print "Cargando autopep8 para elpy")
